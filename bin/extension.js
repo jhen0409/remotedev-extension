e0e3fe61873f014026e6be8626dae19c1a5e1487
Jhen <developer@jhen.me>
2016-02-07 00:15:48 +0800

    Rename bin files

--- bin/extension.js
+++ bin/extension.js
@@ -1,6 +1,29 @@
-#! /usr/bin/env node
+const fs = require('fs');
+const path = require('path');
+const filePath = path.join(__dirname, '../dist/devpanel.tmpl.html');
+const distPath = path.join(__dirname, '../dist/devpanel.html');
+const startRemoteDev = require('remotedev-server');
 
-const argv = require('minimist')(process.argv.slice(2), {
-  boolean: ['runserver']
-});
-require('./server')(argv);
+const html = fs.readFileSync(filePath, 'utf-8');
+
+module.exports = function(argv) {
+  if (argv.runserver) {
+    argv.port = argv.port || 8000;
+    startRemoteDev(argv);
+  }
+  if (argv.hostname || argv.port) {
+    fs.writeFileSync(
+      distPath,
+      html.replace(
+        '__remotedevOptionsSet__',
+        'window.remotedevOptions = ' + JSON.stringify({
+          hostname: argv.hostname,
+          port: argv.port || 8000,
+          autoReconnect: true
+        })
+      )
+    );
+  } else {
+    fs.writeFileSync(distPath, html);
+  }
+};
